07c75d95be0c75a1d703348f278a7c0a201b5d7e
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-23 13:19:30 -0700

    Initial Commit

---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Caleb Harris on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
